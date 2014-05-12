78f914fcc7f045e1f76a2c23a94762a934e8d9eb
Kyle Geske <stungeye@gmail.com>
2014-05-12 16:01:56 -0500

    A few more tweaks to allign with 2014 presentation.

--- adventures_begin/05_inline_styles.rb
+++ adventures_begin/05_inline_styles.rb
@@ -1,6 +1,6 @@
-    Shoes.app :title => 'Q and A' do
-      stack do
-        para strong('Q. '), 'Do you believe in free will?'
-        para em(strong('A. '), 'Nah, dream is destiny.')
-      end
-    end
+Shoes.app :title => 'Q and A' do
+  stack do
+    para strong('Q. '), 'Do you believe in free will?'
+    para em(strong('A. '), 'Nah, dream is destiny.')
+  end
+end
--- adventures_begin/08_image.rb
+++ adventures_begin/08_image.rb
@@ -1,7 +1,7 @@
 Shoes.app :title => 'Soda Pups', :height => 375, :width => 610 do
   flow do
-    @img = image 'pup.png'
-    image 'pup.png' do
+    image 'pup.png'
+    @img = image 'pup.png' do
       @img.path = 'surprised_dog.jpg'
     end
   end
